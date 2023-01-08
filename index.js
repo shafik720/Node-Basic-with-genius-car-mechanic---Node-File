754d0fb295d5c14a136ae1e006bd8c9e1767da97
shafik720 <shafikrasel5@gmail.com>
2023-01-08 20:10:56 +0600

    car db connected

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASSWORD}@cluster0.0ihcm8w.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('genius car db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/', (req, res)=>{
     res.send('hellow world');
 })
