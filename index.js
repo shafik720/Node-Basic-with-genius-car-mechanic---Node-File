97a0c8ed220c988513d84c73965883bf2874c2d9
shafik720 <shafikrasel5@gmail.com>
2023-01-08 20:34:06 +0600

    trying mongodb crud

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -16,25 +16,32 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASSWORD}@cluster0.0ihcm8w.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-        const collection = client.db('Car_Mechanic').collection('Services');
+        const userCollection = client.db('Car_Mechanic').collection('Services');
 
-        const query = {};
-        const cursor = collection.find(query);
-        const services = await cursor.toArray();
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const services = await cursor.toArray();
+
+            res.send(services);
+        })
 
     }
-    finally{}
+    finally { 
+
+    }
 }
 
+run().catch(console.dir);
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('hellow world');
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('connecting to port 5000');
 })
